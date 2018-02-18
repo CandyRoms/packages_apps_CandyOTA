78ac7b32f476ea9d143b47d9f8c1e8e89cf74a5c
xyyx <xyyx@mail.ru>
2018-02-18 11:13:46 +0800

    CandyOTA: rework automatic updates with JobService
    * show statusbar notification when IsBackground
    
    @dadi11:
    * remove deprecated cwac wakeful
    * remove schedulers
    * remove proguard files
    
    Change-Id: Ibe7b60117a713d0f139f4c3941235dfa632f3aba
    Signed-off-by: Bhi768 <bhimanand7@gmail.com>

--- Android.mk
+++ Android.mk
@@ -20,13 +20,11 @@ LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 LOCAL_AAPT_FLAGS := --auto-add-overlay
 
 LOCAL_STATIC_JAVA_LIBRARIES +=\
-    android-support-v4 \
-    libcwac
+    android-support-v4
 
 include $(BUILD_PACKAGE)
 
 include $(CLEAR_VARS)
 
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libcwac:app/libs/cwac-wakeful-1.0.5.jar
 
 include $(BUILD_MULTI_PREBUILT)
